114b10f4f398b4a813d48383b74dac1051a1b7a1
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-06-07 14:06:28 +0200

    initial upload

---
+++ sim_detector_definition.py
@@ -0,0 +1,31 @@
+import os
+from typing import Dict, Callable
+
+from graphnet.models.detector.detector import Detector
+import torch
+
+
+class app_ml_sim(Detector):
+    """Detector class for the simulated setup"""
+
+    geometry_table_path = os.path.join(os.path.dirname(__file__), 'graph_definition.parquet')
+
+    xyz = ['dom_x', 'dom_y', 'dom_z']
+    sensor_id_column = 'dom_id'
+    string_id_column = 'string_id'
+
+    def feature_map(self) -> Dict[str, Callable]:
+        """Map standardization functions to each dimension."""
+        feature_map = {
+            "dom_x": self._sensor_pos_xyz,
+            "dom_y": self._sensor_pos_xyz,
+            "dom_z": self._sensor_pos_xyz,
+            "time": self._t,
+        }
+        return feature_map
+
+    def _sensor_pos_xyz(self, x: torch.tensor) -> torch.tensor:
+        return x
+
+    def _t(self, x: torch.tensor) -> torch.tensor:
+        return x
